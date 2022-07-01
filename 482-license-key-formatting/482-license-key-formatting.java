eaaac54859ff7198ecbad96f49f3ccb1e30021a5
nipa0711 <nipa0711@gmail.com>
2022-07-01 11:57:46 +0900

    Time: 20 ms (57.13%), Space: 45.7 MB (49.53%) - LeetHub

--- 482-license-key-formatting/482-license-key-formatting.java
+++ 482-license-key-formatting/482-license-key-formatting.java
@@ -2,18 +2,17 @@ class Solution {
     public String licenseKeyFormatting(String s, int k) {
         s = s.replace("-","").toUpperCase();
         
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         int count = 0;
         int sLen = s.length();
         for (int i=0; i<sLen; i++) {
             if (count == k) {
-                answer += "-";
+                answer.append("-");
                 count = 0;
-            }     
-            answer += s.charAt(sLen-1-i);
+            }
+            answer.append(s.charAt(sLen-1-i));
             count++;
         }
-        StringBuffer sb = new StringBuffer(answer);
-        return sb.reverse().toString();
+        return answer.reverse().toString();
     }
 }
