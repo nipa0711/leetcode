9f86ebc3308eee555dec714090c17dfc792c7414
nipa0711 <nipa0711@gmail.com>
2022-07-04 10:08:42 +0900

    Time: 21 ms (40.30%), Space: 51.7 MB (31.70%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -6,22 +6,17 @@ class Solution {
             char ch = ransomNote.charAt(i);
             map1.put(ch, map1.getOrDefault(ch, 0) +1);
         }
-        System.out.println("map1 : " +map1);
         
         for (int i=0; i<magazine.length(); i++) {
             char ch = magazine.charAt(i);
             map2.put(ch, map2.getOrDefault(ch, 0) +1);
         }
-        System.out.println("map2 : " +map2);
         
         for(Character key : map1.keySet()) {
-            int value1 = map1.get(key);
             if (!map2.containsKey(key)) {
                 return false;
             }
-            int value2 = map2.get(key);
-            System.out.println("value1 : " + value1 + " value2 : " + value2);
-            if (value1 > value2) {
+            if (map1.get(key) > map2.get(key)) {
                 return false;
             }
         }
