f22adf71444f7e42a7f010eb1ff288d2f82f7a19
nipa0711 <nipa0711@gmail.com>
2022-07-04 12:00:22 +0900

    Time: 77 ms (5.02%), Space: 46.3 MB (94.83%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -2,8 +2,9 @@ import java.math.BigInteger;
 class Solution {
     public int[] countBits(int n) {
         int[] answer = new int[n+1];
+        String str = "";
         for (int i=0; i<=n; i++) {
-            String str = ""+i;
+            str = ""+i;
             BigInteger bigInt = new BigInteger(str);
             str = bigInt.toString(2);
             str = str.replace("0","");
