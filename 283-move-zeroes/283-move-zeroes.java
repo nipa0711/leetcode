dd25063cea3c1a7d444ae05f65884200634db2da
nipa0711 <nipa0711@gmail.com>
2022-07-04 14:36:26 +0900

    Time: 440 ms (5.00%), Space: 54.9 MB (35.62%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,6 +1,9 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         for (int i=0; i<nums.length-1; i++) {
+            if (nums[i] != 0) {
+                continue;
+            }
             for (int j=i+1; j<nums.length; j++) {
                 if (nums[i] == 0 && nums[j] != 0) {
                     int temp = nums[i];
