ba52b789a3343134eab22bf1210ab823de52abbc
nipa0711 <nipa0711@gmail.com>
2022-07-05 17:37:31 +0900

    Time: 2 ms (9.59%), Space: 41 MB (42.54%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.java
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -1,5 +1,6 @@
 class Solution {
     public int mySqrt(int x) {
+        // Code from https://github.com/nipa0711/leetcode/blob/main/69-sqrtx/69-sqrtx.java
         int answer = 0;
         for (double i=1; i <= x; i++) {
             if ((i*i) > x) {
@@ -11,8 +12,7 @@ class Solution {
     }
     
     public boolean isPerfectSquare(int num) {
-        int mine = mySqrt(num);
-        
+        int mine = mySqrt(num);        
         return mine * mine == num;
     }
 }
