87d96b77496f24bb09e64ade991560b607bfce35
nipa0711 <nipa0711@gmail.com>
2022-07-05 16:24:33 +0900

    Time: 25 ms (5.02%), Space: 44.4 MB (16.89%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
@@ -1,5 +1,38 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> hs1 = new HashSet<>();
+        HashSet<Integer> hs2 = new HashSet<>();
+        for (int num : nums1) {
+            hs1.add(num);
+        }
+        for (int num : nums2) {
+            hs2.add(num);
+        }
+        
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        List<Integer> original = new ArrayList<>();
+        
+        Iterator iter = hs1.iterator();
+        while(iter.hasNext()) {
+            int num = (int)iter.next();
+            list1.add(num);
+            original.add(num);
+		}
+        
+        iter = hs2.iterator();
+        while(iter.hasNext()) {
+            list2.add((int)iter.next());
+		}
+        
+        list1.removeAll(list2);
+        original.removeAll(list1);
+        int[] answer = new int[original.size()];
+        for (int i=0; i<original.size(); i++) {
+            answer[i] = original.get(i);
+        }
+        
+        /*
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
         List<Integer> original = new ArrayList<>();
@@ -22,7 +55,7 @@ class Solution {
         int i = 0;
 		while(iter.hasNext()) {
             answer[i++] = (int)iter.next();
-		}
+		}*/
         
         return answer;
     }
