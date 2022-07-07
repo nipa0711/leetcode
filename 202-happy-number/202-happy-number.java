5fdd65cc715066cf4e0f6d15e76615da2249f745
nipa0711 <nipa0711@gmail.com>
2022-07-07 15:23:25 +0900

    Time: 18 ms (5.22%), Space: 45.7 MB (5.33%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -2,9 +2,8 @@ class Solution {
     public boolean isHappy(int n) {
         String str = "" + n;
         Map<String, Integer> map = new HashMap<>();
-        int sum = 0;
         do {
-            sum = 0;
+            int sum = 0;
             char[] arr = str.toCharArray();
             for (int i=0; i<arr.length; i++) {
                 int num = arr[i] - '0';
