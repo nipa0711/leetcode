2dd556b7b57cdf58fdd86839109c8ec202543c8a
nipa0711 <nipa0711@gmail.com>
2022-07-07 10:01:59 +0900

    Time: 2228 ms (5.00%), Space: 299.7 MB (5.06%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,9 +1,7 @@
 class Solution {
     boolean checkPalindrome(String s) {
-        //char[] arr = s.toCharArray();
         for (int i=0; i<s.length()/2; i++) {
             if (s.charAt(i) != s.charAt(s.length()-1-i)) {
-                //System.out.println("arr[i] : " + arr[i] + " arr[arr.length-1-i] : " + arr[arr.length-1-i]);
                 return false;
             }
         }
@@ -23,15 +21,9 @@ class Solution {
                     break;
                 }
                 String parse = s.substring(i, s.length()-j);
-                //System.out.println("parse : " + parse);
                 if (checkPalindrome(parse) && parse.length() > answer.length()) {
                     answer = parse;
                 }
-                //sb = new StringBuilder(parse);
-                //String reverse = sb.reverse().toString();
-               // if (parse.equals(reverse) && parse.length() > answer.length()) {
-                //    answer = parse;
-               // }
             }
         }
         return answer;
