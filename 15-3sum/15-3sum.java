35db6cffb0b8e4cc44b31cacf13f5e7421e52b67
nipa0711 <nipa0711@gmail.com>
2022-07-08 17:06:15 +0900

    Time: 26 ms (85.61%), Space: 59.1 MB (51.94%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -20,15 +20,12 @@ class Solution {
                     list.add(nums[first]);
                     list.add(nums[second]);
                     list.add(nums[last]);
-                    //Collections.sort(list);
-                    //if (!answer.contains(list)) {
-                        answer.add(list);
-                    //}
+                    answer.add(list);
                     last--;
-                    while(second < last && nums[second] == nums[second-1]) {
+                    while(second < last && nums[second] == nums[second - 1]) {
                         second++;
                     }
-                    while(second < last && nums[last] == nums[last+1]) {
+                    while(second < last && nums[last] == nums[last + 1]) {
                         last--;
                     }
                 } else if (sum < 0) {
