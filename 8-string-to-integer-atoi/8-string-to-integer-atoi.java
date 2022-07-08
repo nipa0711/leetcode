ed6b605e4f066c33d9e5dd1e7ac7be6387a15bb8
nipa0711 <nipa0711@gmail.com>
2022-07-08 15:10:27 +0900

    Time: 22 ms (5.40%), Space: 44.8 MB (5.41%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -0,0 +1,40 @@
+import java.math.BigInteger;
+class Solution {
+    public int myAtoi(String s) {
+        boolean needContinue = true;
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<s.length(); i++) {
+            int num = s.charAt(i);
+            if (num == 32 && needContinue) {
+                continue;
+            } else if (num == 43 || num == 45) {
+                if (!needContinue) {
+                    break;
+                }
+                sb.append((char)num);
+                needContinue = false;
+                continue;
+            }
+            if (num >= 48 && num <= 57) {
+                sb.append((char)num);
+                needContinue = false;
+            } else {
+                break;
+            }
+        }
+        System.out.println("sb.toString() : " + sb.toString());
+        
+        try {
+            BigInteger bigInt = new BigInteger(sb.toString());
+            if (bigInt.compareTo(new BigInteger("" + Integer.MAX_VALUE)) == 1) {
+                return Integer.MAX_VALUE;
+            } else if (bigInt.compareTo(new BigInteger("" + Integer.MIN_VALUE)) == -1) {
+                return Integer.MIN_VALUE;
+            }
+            System.out.println("test : " + bigInt.toString());
+            return Integer.parseInt(bigInt.toString());
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+}
