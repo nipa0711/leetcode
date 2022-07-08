ddb779fcdc443f184d9b34ab49c1c5164df51870
nipa0711 <nipa0711@gmail.com>
2022-07-08 09:39:25 +0900

    Time: 1 ms (99.91%), Space: 54.2 MB (74.30%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,6 +1,10 @@
 class Solution {
     public void reverseString(char[] s) {
         for (int i=0; i<s.length/2; i++) {
+            char ch = s[i];
+            s[i] = s[s.length-1-i];
+            s[s.length-1-i] = ch;
+            /*
             int a = s[i];
             int b = s[s.length-1-i];
             a = a + b;
@@ -8,6 +12,7 @@ class Solution {
             a = a - b;
             s[i] = (char)a;
             s[s.length-1-i] = (char)b;
+            */
         }
     }
 }
