ce4101f3d21d99895bd6a68c3441545f9be924f2
nipa0711 <nipa0711@gmail.com>
2022-07-15 12:26:08 +0900

    Time: 0 ms (100.00%), Space: 40.7 MB (95.78%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.java
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.java
@@ -7,38 +7,23 @@ class Solution {
                 continue;
             }
             
-            int s1=0;
-            int s2=0;
-            //boolean check = true;
+            int s1 = 0;
+            int s2 = 0;
             while (s1<word1.length() && s2 < word2.length()) {
                 char ch1 = word1.charAt(s1++);
                 char ch2 = word2.charAt(s2++);
                 if (ch1 == ch2) {
                     continue;
                 } else if (order.indexOf(ch1) > order.indexOf(ch2)) {
-                    System.out.println("false here1");
                     return false;
                 } else {
-                    //check = false;
                     break;
                 }
             }
             
             if (word1.indexOf(word2) != -1) {
-                System.out.println("false here2");
                 return false;
-            }
-            /*
-            if (check) {
-                while (s2 < word2.length()) {
-                    char ch1 = word2.charAt(s2-1);
-                    char ch2 = word2.charAt(s2++);
-                    if (order.indexOf(ch1) > order.indexOf(ch2)) {
-                        System.out.println("false here3");
-                        return false;
-                    }        
-                }
-            } */                       
+            }                   
         }
         
         return true;
