48301caf1df813fdd20d73d508ea5f0d37629be8
nipa0711 <nipa0711@gmail.com>
2022-07-18 17:58:07 +0900

    Time: 42 ms (5.20%), Space: 41.3 MB (64.92%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        double answer = 0;
+        for (int i=1; i<=n; i++) {
+            answer += i;
+            if (answer > n) {
+                answer = i - 1;
+                break;
+            } else if (answer == n) {
+                answer = i;
+                break;
+            }
+        }
+        return (int)answer;
+    }
+}
