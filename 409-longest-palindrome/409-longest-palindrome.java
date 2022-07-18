6d3d3dce87777e507a29b0e32b6ca31415d442e5
nipa0711 <nipa0711@gmail.com>
2022-07-18 17:21:22 +0900

    Time: 12 ms (15.42%), Space: 42 MB (77.63%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        
+        int answer = 0;
+        boolean once = false;
+        for (int value : map.values()) {
+            if (value % 2 == 0) {
+                answer += value;
+            } else if (value % 2 == 1 && value > 1) {
+                answer += (value -1);
+                once = true;
+            } else if (value == 1) {
+                once = true;
+            }
+        }
+        if (once) {
+            answer++;
+        }
+        
+        System.out.println("answer : " + answer);
+        System.out.println(map);
+        return answer;
+    }
+}
