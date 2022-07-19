0fdc8143ab99b6b0d6394560d92cb2fadc2ab269
nipa0711 <nipa0711@gmail.com>
2022-07-19 16:14:31 +0900

    Time: 19 ms (52.74%), Space: 50.3 MB (60.54%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -3,8 +3,7 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i=0; i<nums.length; i++) {
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        System.out.println("map : " + map);        
+        }   
         
         List<Map.Entry<Integer, Integer>> entryList = new LinkedList<>(map.entrySet());
         entryList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
@@ -12,7 +11,6 @@ class Solution {
         int index = 0;
         int[] answer = new int[k];
         for(Map.Entry<Integer, Integer> entry : entryList) {
-            System.out.println("key : " + entry.getKey() + ", value : " + entry.getValue());
             answer[index] = entry.getKey();
             index++;
             if (index>=k) {
