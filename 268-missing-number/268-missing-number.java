7bae21a2e03bc423a116f4035114c3167336c3e2
nipa0711 <nipa0711@gmail.com>
2022-07-20 09:39:37 +0900

    Time: 1 ms (73.27%), Space: 50.7 MB (70.32%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        boolean[] check = new boolean[nums.length+1];
+        for (int num : nums) {
+            check[num] = true;
+        }
+        for (int i=0; i<check.length; i++) {
+            if (!check[i]) {
+                return i;
+            }
+        }
+        return check.length;
+    }
+}
