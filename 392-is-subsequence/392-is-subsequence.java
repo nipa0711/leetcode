140eca40690b4c0f702f03d2e6cc0df7fa9f5ead
nipa0711 <nipa0711@gmail.com>
2022-07-21 18:27:27 +0900

    Time: 0 ms (100.00%), Space: 42 MB (49.50%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -8,20 +8,12 @@ class Solution {
             return false;
         }
         
-        //int saved = 0;
         for (int i = 0; i < s.length(); i++) {
             try {
                 int index = t.indexOf(s.charAt(i));
-                ///System.out.println("index : " + index);
-                //System.out.println("t : " + t);
                 if (index == -1) {
                     return false;
                 }
-                /*
-                if (saved > index) {
-                    return false;
-                }
-                saved = index;*/
                 t = t.substring(index + 1);
             } catch (Exception e) {
                 return false;
