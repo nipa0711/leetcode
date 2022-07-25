a7dec84619c12f55d18c17e975a29c5921b88f2d
nipa0711 <nipa0711@gmail.com>
2022-07-25 12:36:34 +0900

    Time: 234 ms (20.55%), Space: 42.7 MB (93.52%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        boolean[] visited = new boolean[nums.length];
+        Queue<Integer> queue = new LinkedList<>();
+        visited[0] = true;
+        queue.add(0);
+        
+        while(!queue.isEmpty()) {
+            int index = queue.poll();
+            for (int i=1; i<=nums[index]; i++) {
+                int newIdx = index + i;
+                
+                if (!(0 <= newIdx && newIdx < nums.length) || visited[newIdx]) {
+                    continue;
+                }                    
+                
+                if (newIdx == nums.length - 1) {
+                    return true;
+                }                    
+                
+                visited[newIdx] = true;
+                queue.add(newIdx);
+            }
+        }
+        
+        return visited[nums.length-1];
+    }
+}
