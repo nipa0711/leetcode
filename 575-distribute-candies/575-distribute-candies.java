a13d1105489f0475696bafe22c037fa2a86c479b
nipa0711 <nipa0711@gmail.com>
2022-07-28 18:14:37 +0900

    Time: 69 ms (36.07%), Space: 112.1 MB (14.63%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.java
+++ 575-distribute-candies/575-distribute-candies.java
@@ -1,10 +1,17 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
+        /*
         Map<Integer, Integer> map = new HashMap<>();
         for (int candy : candyType) {
             map.put(candy, map.getOrDefault(candy, 0) + 1);
         }
         
         return Math.min(candyType.length/2, map.size());
+        */
+        Set<Integer> set = new HashSet();
+        for (int candy : candyType) {
+            set.add(candy);
+        }
+        return Math.min(candyType.length/2, set.size());
     }
 }
