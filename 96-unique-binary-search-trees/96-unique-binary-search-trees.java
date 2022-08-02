a80ed3c8e8c039aee79cbe4f8a8bb9fd2abfe206
nipa0711 <nipa0711@gmail.com>
2022-08-02 17:46:42 +0900

    Time: 6 ms (5.06%), Space: 41.2 MB (26.91%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -0,0 +1,18 @@
+import java.math.BigDecimal;
+class Solution {
+    // http://oeis.org/search?q=1%2C2%2C5%2C14%2C42%2C132%2C429%2C1430%2C4862&language=english&go=Search
+    // Catalan numbers: C(n) = binomial(2n,n)/(n+1) = (2n)!/(n!(n+1)!).
+    public int numTrees(int n) {
+        BigDecimal[] factorial = new BigDecimal[2*n+1];
+        factorial[0] = new BigDecimal(1);
+        factorial[1] = new BigDecimal(1);
+        for (int i=2; i<=2*n; i++) {
+            factorial[i] = factorial[i-1].multiply(new BigDecimal(i));
+        }
+        
+        BigDecimal up = factorial[2*n];
+        BigDecimal down = factorial[n].multiply(factorial[n+1]);
+        
+        return Integer.parseInt(up.divide(down).toString());
+    }
+}
