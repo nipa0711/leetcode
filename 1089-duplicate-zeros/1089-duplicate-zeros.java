910d9092bf5421743e462dd036491df289a169f3
nipa0711 <nipa0711@gmail.com>
2022-08-03 18:06:28 +0900

    Time: 7 ms (49.52%), Space: 46.6 MB (7.61%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -6,6 +6,9 @@ class Solution {
                 queue.add(0);
             }
             queue.add(num);
+            if (queue.size() == arr.length) {
+                break;
+            }
         }
         
         for (int i=0; i<arr.length; i++) {
