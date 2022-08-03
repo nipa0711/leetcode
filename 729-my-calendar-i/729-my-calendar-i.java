770b1de52006a675d516973d25a87e0ff1f97ec1
nipa0711 <nipa0711@gmail.com>
2022-08-03 11:58:27 +0900

    Time: 129 ms (19.08%), Space: 54.7 MB (44.49%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -0,0 +1,24 @@
+class MyCalendar {
+    
+    List<int[]> list;
+
+    public MyCalendar() {
+        list = new ArrayList<>();
+    }
+    
+    public boolean book(int start, int end) {
+        for (int[] arr: list) {
+            if (arr[0] < end && start < arr[1]) {
+                return false;
+            }
+        }
+        list.add(new int[]{start, end});
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
