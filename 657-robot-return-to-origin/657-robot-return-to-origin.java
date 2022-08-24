07aca61a9560a682ee965c5fe3868ff724d8f0b7
nipa0711 <nipa0711@gmail.com>
2022-08-24 11:16:22 +0900

    Time: 20 ms (5.70%), Space: 49.6 MB (5.07%) - LeetHub

---
+++ 657-robot-return-to-origin/657-robot-return-to-origin.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i=0; i<moves.length(); i++) {
+            char ch = moves.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        
+        if (map.containsKey('U') != map.containsKey('D')) {
+            return false;
+        } else {
+            if (map.get('U') != null && map.get('D') != null) {
+                if (!map.get('U').equals(map.get('D'))) {
+                    return false;
+                }
+            } 
+        }
+        if (map.containsKey('L') != map.containsKey('R')) {
+            return false;
+        } else {
+            if (map.get('L') != null && map.get('R') != null) {
+                if (!map.get('L').equals(map.get('R'))) {
+                    return false;
+                }
+            }            
+        }
+        return true;
+    }
+}
