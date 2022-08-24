a5a383075e3ae47672e755e80c939fd16da07e93
nipa0711 <nipa0711@gmail.com>
2022-08-24 11:41:14 +0900

    Time: 6 ms (87.71%), Space: 44.4 MB (80.98%) - LeetHub

--- 657-robot-return-to-origin/657-robot-return-to-origin.java
+++ 657-robot-return-to-origin/657-robot-return-to-origin.java
@@ -1,27 +1,24 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        Map<Character, Integer> map = new HashMap<>();
+        int udNum = 0;
+        int lrNum = 0;
         for (int i=0; i<moves.length(); i++) {
-            char ch = moves.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-        
-        if (map.containsKey('U') != map.containsKey('D')) {
-            return false;
-        } else if (map.get('U') != null && map.get('D') != null) {
-            if (!map.get('U').equals(map.get('D'))) {
-                return false;
+            switch (moves.charAt(i)) {
+                case 'U':
+                    udNum++;
+                    break;
+                case 'D':
+                    udNum--;
+                    break;
+                case 'L':
+                    lrNum++;
+                    break;
+                case 'R':
+                    lrNum--;
+                    break;
             }
         }
         
-        if (map.containsKey('L') != map.containsKey('R')) {
-            return false;
-        } else if (map.get('L') != null && map.get('R') != null) {
-            if (!map.get('L').equals(map.get('R'))) {
-                return false;
-            }
-        }
-        
-        return true;
+        return udNum == 0 && lrNum == 0;
     }
 }
