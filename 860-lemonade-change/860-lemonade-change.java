534fb780923e3b38c778b9baa075eaa82553add6
nipa0711 <nipa0711@gmail.com>
2022-08-29 14:30:15 +0900

    Time: 441 ms (5.21%), Space: 116.7 MB (6.73%) - LeetHub

---
+++ 860-lemonade-change/860-lemonade-change.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(5, 0);
+        map.put(10, 0);
+        map.put(20, 0);
+        
+        for (int money : bills) {
+            System.out.println("money : " + money);
+            System.out.println("map : " + map);
+            switch (money) {
+                case 5:
+                    map.put(money, map.getOrDefault(money, 0) +1);
+                    break;
+                case 10:
+                    if (map.get(5) <= 0) {
+                        return false;
+                    }
+                    map.put(money, map.getOrDefault(money, 0) +1);
+                    map.put(5, map.getOrDefault(5, 0) -1);
+                    break;
+                case 20:
+                    if (map.get(5) <= 0) {
+                        return false;
+                    }
+                    if (map.get(10) <= 0 && map.get(5) <= 0) {
+                        return false;
+                    } else if (map.get(5)>=3 && map.get(10)==0) {
+                        map.put(5, map.getOrDefault(5, 0) -3);
+                    } else {
+                        map.put(10, map.getOrDefault(10, 0) -1);
+                        map.put(5, map.getOrDefault(5, 0) -1);
+                    }
+                    
+                    if (map.get(10) < 0) {
+                        return false;
+                    } 
+                    map.put(money, map.getOrDefault(money, 0) +1);                    
+                    break;
+            }
+        }
+        return true;
+    }
+}
