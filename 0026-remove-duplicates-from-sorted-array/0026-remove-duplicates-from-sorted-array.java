421d4b249054233eeb5e67b7c9f4fa25371ce17c
nipa0711 <nipa0711@gmail.com>
2022-11-21 11:05:12 +0900

    Time: 161 ms (5.04%), Space: 48 MB (39.99%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,18 +3,19 @@ class Solution {
         
         int count = 1;
         int pre = nums[0];
-        for (int i=1; i<nums.length; i++) {
+        int size = nums.length;
+        for (int i=1; i<size; i++) {
             int current = nums[i];
             if (pre<current) {
                 pre = current;
                 count++;
             }
         }
-        for (int i=0; i<nums.length-1; i++) {
+        for (int i=0; i<size-1; i++) {
             int index = i + 1;
             int now = nums[i];            
             
-            for (int j=i+1; j<nums.length; j++) {
+            for (int j=i+1; j<size; j++) {
                 int next = nums[j];
                 if (now < next) {
                     nums[index] = next;
